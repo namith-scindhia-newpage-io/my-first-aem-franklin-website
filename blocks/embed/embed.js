5c718c140b6a50465516ff8574b52373947a5889
namith-scindhia-newpage-io <namith.scindhia@newpage.io>
2023-12-18 14:11:00 +0530

    fix lint

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -5,11 +5,11 @@
  */
 
 const loadScript = (url, callback, type) => {
-  const head = document.querySelector("head");
-  const script = document.createElement("script");
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
   script.src = url;
   if (type) {
-    script.setAttribute("type", type);
+    script.setAttribute('type', type);
   }
   script.onload = callback;
   head.append(script);
@@ -26,11 +26,11 @@ const getDefaultEmbed = (
 
 const embedYoutube = (url, autoplay) => {
   const usp = new URLSearchParams(url.search);
-  const suffix = autoplay ? "&muted=1&autoplay=1" : "";
-  let vid = usp.get("v") ? encodeURIComponent(usp.get("v")) : "";
+  const suffix = autoplay ? '&muted=1&autoplay=1' : '';
+  let vid = usp.get('v') ? encodeURIComponent(usp.get('v')) : '';
   const embed = url.pathname;
-  if (url.origin.includes("youtu.be")) {
-    [, vid] = url.pathname.split("/");
+  if (url.origin.includes('youtu.be')) {
+    [, vid] = url.pathname.split('/');
   }
   const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
         <iframe src="https://www.youtube.com${
@@ -42,8 +42,8 @@ const embedYoutube = (url, autoplay) => {
 };
 
 const embedVimeo = (url, autoplay) => {
-  const [, video] = url.pathname.split("/");
-  const suffix = autoplay ? "?muted=1&autoplay=1" : "";
+  const [, video] = url.pathname.split('/');
+  const suffix = autoplay ? '?muted=1&autoplay=1' : '';
   const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
         <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
         style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
@@ -60,21 +60,21 @@ const embedTwitter = (url) => {
 };
 
 const loadEmbed = (block, link, autoplay) => {
-  if (block.classList.contains("embed-is-loaded")) {
+  if (block.classList.contains('embed-is-loaded')) {
     return;
   }
 
   const EMBEDS_CONFIG = [
     {
-      match: ["youtube", "youtu.be"],
+      match: ['youtube', 'youtu.be'],
       embed: embedYoutube,
     },
     {
-      match: ["vimeo"],
+      match: ['vimeo'],
       embed: embedVimeo,
     },
     {
-      match: ["twitter"],
+      match: ['twitter'],
       embed: embedTwitter,
     },
   ];
@@ -88,23 +88,23 @@ const loadEmbed = (block, link, autoplay) => {
     block.classList = `block embed embed-${config.match[0]}`;
   } else {
     block.innerHTML = getDefaultEmbed(url);
-    block.classList = "block embed";
+    block.classList = 'block embed';
   }
-  block.classList.add("embed-is-loaded");
+  block.classList.add('embed-is-loaded');
 };
 
 export default function decorate(block) {
-  const placeholder = block.querySelector("picture");
-  const link = block.querySelector("a").href;
-  block.textContent = "";
+  const placeholder = block.querySelector('picture');
+  const link = block.querySelector('a').href;
+  block.textContent = '';
 
   if (placeholder) {
-    const wrapper = document.createElement("div");
-    wrapper.className = "embed-placeholder";
+    const wrapper = document.createElement('div');
+    wrapper.className = 'embed-placeholder';
     wrapper.innerHTML =
       '<div class="embed-placeholder-play"><button title="Play"></button></div>';
     wrapper.prepend(placeholder);
-    wrapper.addEventListener("click", () => {
+    wrapper.addEventListener('click', () => {
       loadEmbed(block, link, true);
     });
     block.append(wrapper);
