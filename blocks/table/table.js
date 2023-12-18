0b4144e68d65b6bfda31b849ff05fe70b8bcd92d
namith-scindhia-newpage-io <namith.scindhia@newpage.io>
2023-12-18 13:21:45 +0530

    Fix some more lint issues

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -6,19 +6,19 @@
 
 function buildCell(rowIndex) {
   const cell = rowIndex
-    ? document.createElement("td")
-    : document.createElement("th");
-  if (!rowIndex) cell.setAttribute("scope", "col");
+    ? document.createElement('td')
+    : document.createElement('th');
+  if (!rowIndex) cell.setAttribute('scope', 'col');
   return cell;
 }
 
 export default async function decorate(block) {
-  const table = document.createElement("table");
-  const thead = document.createElement("thead");
-  const tbody = document.createElement("tbody");
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
   table.append(thead, tbody);
   [...block.children].forEach((child, i) => {
-    const row = document.createElement("tr");
+    const row = document.createElement('tr');
     if (i) tbody.append(row);
     else thead.append(row);
     [...child.children].forEach((col) => {
@@ -27,6 +27,6 @@ export default async function decorate(block) {
       row.append(cell);
     });
   });
-  block.innerHTML = "";
+  block.innerHTML = '';
   block.append(table);
 }
