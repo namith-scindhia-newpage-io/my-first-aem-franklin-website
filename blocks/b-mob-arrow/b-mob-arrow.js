a7ca0507128ad76b901ba0a441e6c9ad893c90fe
namith-scindhia-newpage-io <namith.scindhia@newpage.io>
2023-12-26 15:26:00 +0530

    address review comments

--- blocks/b-mob-arrow/b-mob-arrow.js
+++ blocks/b-mob-arrow/b-mob-arrow.js
@@ -1,16 +1,15 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  block.classList.add('d-flex', 'flex-column', 'flex-lg-row');
+  block.classList.add('d-flex', 'flex-column', 'flex-lg-row', 'common-menu');
 
   [...block.children].forEach((row) => {
     row.classList.add('d-flex', 'p-3', 'align-items-center');
     const children = [...row.children];
 
     const pictureDiv = children[0];
-    const titleDiv = children[1];
-    const link = children[2];
-    const colorCode = children[3];
+    const linkButton = children[1];
+    const colorCode = children[2];
 
     pictureDiv.classList.add('flex-shrink-0');
     pictureDiv
@@ -24,27 +23,18 @@ export default function decorate(block) {
       );
 
     const triangle1 = document.createElement('div');
-    triangle1.classList.add('triangle', 'ms-1', 'me-1');
+    triangle1.classList.add('triangle', 'ms-2', 'me-1');
     triangle1.style['border-left-color'] = colorCode.getInnerHTML();
 
     const triangle2 = document.createElement('div');
     triangle2.classList.add('triangle');
     triangle2.style['border-left-color'] = colorCode.getInnerHTML();
 
-    const anchor = document.createElement('a');
-    anchor.classList.add('title');
-    anchor.setAttribute('href', link.getInnerHTML());
-    anchor.innerHTML = titleDiv.getInnerHTML();
+    linkButton.append(triangle1);
+    linkButton.append(triangle2);
 
-    const divContainer = document.createElement('div');
-    divContainer.classList.add('ms-3');
-    divContainer.append(anchor);
+    linkButton.classList.add('common-menu-text');
 
-    divContainer.append(triangle1);
-    divContainer.append(triangle2);
-
-    titleDiv.replaceWith(divContainer);
-    link.remove();
     colorCode.remove();
 
     const divhr = document.createElement('div');
