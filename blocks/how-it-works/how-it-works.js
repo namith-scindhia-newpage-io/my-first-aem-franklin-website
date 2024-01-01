70db4b693ee6ff72049004a37651f05f3747304d
namith-scindhia-newpage-io <namith.scindhia@newpage.io>
2024-01-01 15:23:22 +0530

    how it works block

---
+++ blocks/how-it-works/how-it-works.js
@@ -0,0 +1,55 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  block.classList.add('d-flex', 'flex-column', 'flex-lg-row');
+
+  [...block.children].forEach((row, index) => {
+    row.classList.add(
+      'd-flex',
+      'flex-column',
+      'align-items-center',
+      'how-it-works-column',
+      `column-${index + 1}`
+    );
+    const children = [...row.children];
+    const pictureDiv = children[0];
+    const textContentDiv = children[1];
+
+    const iconContainer = document.createElement('div');
+    iconContainer.classList.add('how-it-works-icon', 'd-flex');
+
+    pictureDiv
+      .querySelectorAll('img')
+      .forEach((img) =>
+        iconContainer.append(
+          createOptimizedPicture(img.src, img.alt, false, [{ width: '110' }])
+        )
+      );
+
+    const numericalDiv = document.createElement('div');
+    numericalDiv.classList.add('how-it-works-number', 'my-4');
+    numericalDiv.innerHTML = index + 1;
+
+    const imagesContainer = document.createElement('div');
+    imagesContainer.classList.add(
+      'how-it-works-image-conatiner',
+      'flex-column',
+      'd-flex',
+      'align-items-center'
+    );
+
+    imagesContainer.append(iconContainer);
+    imagesContainer.append(numericalDiv);
+
+    const columnWrapper = document.createElement('div');
+    columnWrapper.classList.add('how-it-works-column-wrapper');
+    columnWrapper.append(imagesContainer);
+
+    columnWrapper.append(imagesContainer);
+
+    textContentDiv.classList.add('how-it-works-content');
+    columnWrapper.append(textContentDiv);
+
+    row.innerHTML = columnWrapper.outerHTML;
+  });
+}
